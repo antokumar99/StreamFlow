239fc6edf9fc23d7470dc74e6b07c60905690c14
antokumar99 <antokumar101@gmail.com>
2026-05-13 05:09:40 +0600

    clint side created

---
+++ apps/client/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-zinc-900">
+   
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+        Welcome to the Client App
+      </h1>
+      <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
+        This is the home page of the client application.
+      </p>
+    </div>
+  );
+}
