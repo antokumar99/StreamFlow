dd7c0020560c8275d46423ffacb62aad7c1e7a86
antokumar99 <antokumar101@gmail.com>
2026-05-13 23:23:30 +0600

    login and registation page done

---
+++ apps/client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "@/components/auth/AuthLayout";
+import LoginForm from "@/components/auth/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <AuthLayout
+      title="Welcome Back!"
+      subtitle="Login to continue your meeting">
+      <LoginForm />
+    </AuthLayout>
+  );
+}
