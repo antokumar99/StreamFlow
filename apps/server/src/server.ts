9696f4bac4a74aed5788dbf0b44389b72bbd025e
antokumar99 <antokumar101@gmail.com>
2026-05-17 05:06:48 +0600

    modified file

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,17 +1,22 @@
 import dotenv from "dotenv";
-import User from "./models/User.model";
+// import User from "./models/User.model";
 dotenv.config();
 
 
-import app from "./app";
+// import app from "./app";
 import { connectDB } from "./config/db";
+import server from "./config/socket";
 
 const startServer = async () =>
 {
     await connectDB();
-    app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});};
+    // app.listen(process.env.PORT, () => {
+    //     console.log(`Server is running on port ${process.env.PORT}`);
+    // });
+    server.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+};
 
 startServer();
 
--- apps/server/src/utils/generateToken.ts
+++ apps/server/src/utils/generateToken.ts
@@ -11,3 +11,15 @@ export const generateToken = (
     }
   );
 };
+
+export const verifyToken = (
+  token: string
+) => {
+  return jwt.verify(
+    token,
+    process.env.JWT_SECRET as string
+
+  ) as { 
+    id: string 
+  };
+};
